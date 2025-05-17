b6a9368e0bb7ca9b11f774df166f9d7c21763850
Willy Yeremi <willyyeremi@gmail.com>
2025-05-17 18:28:30 +0700

    build:

--- main_v1.py
+++ main_v1.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from routes import data_bp
+from flask_jwt_extended import JWTManager
 
+from routes import data_bp
 from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(data_bp)
+
+jwt = JWTManager()
+jwt.init_app(app)
 
 
 if __name__ == "__main__":
